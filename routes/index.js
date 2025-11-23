1c2a222a9b35c20e8600996343c54bd6fd01c1e6
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2025-11-23 11:48:54 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+// routes/index.js
 const express = require('express');
 const router = express.Router();
 
@@ -5,10 +6,12 @@ const router = express.Router();
 const healthRoutes = require('./health');
 const phoneOtpRoutes = require('./phone-otp');
 const emailOtpRoutes = require('./email-otp');
+const googleOauthRoutes = require('./google-oauth'); // Add this line
 
 // Use routes
 router.use(healthRoutes);
 router.use(phoneOtpRoutes);
 router.use(emailOtpRoutes);
+router.use(googleOauthRoutes); // Add this line
 
 module.exports = router;
